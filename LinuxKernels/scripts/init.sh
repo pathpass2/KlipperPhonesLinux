3b55a61caf4dc431d5eabf72938f3d0ba715e58f
pathpass2 <pathpass2@163.com>
2025-06-07 19:45:49 +0800

    Update init.sh
    Ubuntu22.04下编译，安装依赖

--- LinuxKernels/scripts/init.sh
+++ LinuxKernels/scripts/init.sh
@@ -1,5 +1,8 @@
 echo "正在安装依赖，请确保系统为Ubuntu20.04"
-sudo apt install binfmt-support qemu-user-static gcc-10-aarch64-linux-gnu kernel-package fakeroot simg2img img2simg mkbootimg bison flex gcc-aarch64-linux-gnu pkg-config libncurses-dev libssl-dev unzip git rsync
+#sudo apt install binfmt-support qemu-user-static gcc-10-aarch64-linux-gnu kernel-package fakeroot simg2img img2simg mkbootimg bison flex gcc-aarch64-linux-gnu pkg-config libncurses-dev libssl-dev unzip git rsync
+
+# Ubuntu-22.04需要将kernel-package替换为linux-headers-generic build-essential
+sudo apt install binfmt-support qemu-user-static gcc-10-aarch64-linux-gnu linux-headers-generic build-essential fakeroot simg2img img2simg mkbootimg bison flex gcc-aarch64-linux-gnu pkg-config libncurses-dev libssl-dev unzip git rsync
 
 # echo "正在下载内核源码(6.7.5)"
 # git clone https://gitee.com/meiziyang2023/linux.git
